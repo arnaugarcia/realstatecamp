1c729bb41b641b44b6ec29b73e3f945e1ce3c6f0
Brian Calvo <brian.calvo.aragon@gmail.com>
2017-03-13 20:21:30 +0100

    Translations // #12
                           - Home page translation done.
                           - T0D0 function go to top for the moment inhabilitated.

--- src/main/webapp/content/js/script.js
+++ src/main/webapp/content/js/script.js
@@ -168,10 +168,5 @@ $(document).ready(function(){
              }
         });
     });
-    /* Go to top function */
-    $('#top').click(function() {      // When arrow is clicked
-        $('body,html').animate({
-            scrollTop : 0                       // Scroll to top of body
-        }, 500);
-    });
 });
+
